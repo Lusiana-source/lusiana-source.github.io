e1517d6c1adfe24ee0cedb3cb73ef9b7795d5577
Lusiana <lusianabanne49@gmail.com>
2025-06-03 18:12:25 -1200

    update halaman create

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,38 +11,36 @@
 
     <div class="flex min-h-screen">
 
-        <!-- Sidebar -->
-        <aside class="w-64 bg-gray-800 text-white flex flex-col p-6 space-y-4">
-
-        
-            <div class="text-2xl font-bold mb-4">
-                <span class="text-blue-400">Little</span> Scndh
-            </div>
-            <nav class="flex-1">
-                <ul class="space-y-2">
-                    <li>
-                        <a href="{{ url('/admin/dashboard') }}" class="block px-4 py-2 rounded hover:bg-gray-700 transition">
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/admin/products') }}" class="block px-4 py-2 rounded hover:bg-gray-700 transition">
-                            Kelola Produk
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <div>
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit" class="block w-full text-center bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded transition">
-        Logout
-    </button>
-</form>
-
-            </div>
-        </aside>
-
+      <!-- Sidebar -->
+@if (!request()->is('admin/products/create'))
+<aside class="w-64 bg-gray-800 text-white flex flex-col p-6 space-y-4">
+    <div class="text-2xl font-bold mb-4">
+        <span class="text-blue-400">Little</span> Scndh
+    </div>
+    <nav class="flex-1">
+        <ul class="space-y-2">
+            <li>
+                <a href="{{ url('/admin/dashboard') }}" class="block px-4 py-2 rounded hover:bg-gray-700 transition">
+                    Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="{{ url('/admin/products') }}" class="block px-4 py-2 rounded hover:bg-gray-700 transition">
+                    Kelola Produk
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="block w-full text-center bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded transition">
+                Logout
+            </button>
+        </form>
+    </div>
+</aside>
+@endif
         <!-- Main Content -->
         <main class="flex-1 p-6 overflow-y-auto">
 
